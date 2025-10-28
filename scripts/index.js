dfdaf428feebf7f6ab375a12289f4bdb7631d6a3
leeseyeonn <zzin8520@gmail.com>
2025-10-28 18:13:44 +0900

    이벤트박스

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+
+/* 1행 광고 슬라이드 */
 const eventSwiper = new Swiper("#event_add", {
   slidesPerView: "1.2",
   spaceBetween: 60,
@@ -13,14 +15,32 @@ const eventSwiper = new Swiper("#event_add", {
   },
 });
 
-const newItems = new Swiper('#new_items_wrap',{
+
+/* 2행 신상품 슬라이드 */
+const newItems = new Swiper('#new_wrap #new_items_wrap',{
   slidesPerView: "5",
   spaceBetween: 10,
+  loop: true,
   navigation: {
-    nextEl: "#new_items_wrap .swiper-button-next",
-    prevEl: "#new_items_wrap .swiper-button-prev",
+    nextEl: "#new_wrap .swiper_btn_grp .swiper-button-next",
+    prevEl: "#new_wrap .swiper_btn_grp .swiper-button-prev",
   },
   scrollbar: {
-    el: "#new_items_wrap .swiper-scrollbar",
+    el: "#new_wrap .swiper-scrollbar",
+  },
+})
+
+/* 3행 인기제품 슬라이드 */
+
+const hotItems = new Swiper ('#hot_wrap #hot_items_wrap',{
+  slidesPerView: 7,
+  centeredSlides: true,
+  loop: true,
+  navigation: {
+    nextEl: "#hot_wrap .swiper_btn_grp .swiper-button-next",
+    prevEl: "#hot_wrap .swiper_btn_grp .swiper-button-prev",
+  },
+  scrollbar: {
+    el: "#hot_wrap .swiper-scrollbar",
   },
 })
