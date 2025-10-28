c6f71b2ce34d2194281e8bd732b0934153874713
leeseyeonn <zzin8520@gmail.com>
2025-10-28 09:31:20 +0900

    2행

--- scripts/index.js
+++ scripts/index.js
@@ -12,3 +12,15 @@ const eventSwiper = new Swiper("#event_add", {
     prevEl: "#event_add .add_btn .swiper-button-prev",
   },
 });
+
+const newItems = new Swiper('#new_items_wrap',{
+  slidesPerView: "5",
+  spaceBetween: 10,
+  navigation: {
+    nextEl: "#new_items_wrap .swiper-button-next",
+    prevEl: "#new_items_wrap .swiper-button-prev",
+  },
+  scrollbar: {
+    el: "#new_items_wrap .swiper-scrollbar",
+  },
+})
